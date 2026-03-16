cf595611036381f8d785653afa593885fbe060c4
Mayank Rana <mr9536893@gmail.com>
2026-03-16 21:34:53 +0530

    [Mayank Rana]

--- src/com/gla/pakagesQuestions/Q1/college/faculty/faculty.java
+++
@@ -1,8 +0,0 @@
-package com.gla.packagesQuestions.Q1.college.faculty;
-
-public class faculty {
-    public void display(){
-        System.out.println("Faculty: Raj Kumar");
-        System.out.println("Subject: Java");
-    }
-}
--- src/com/gla/pakagesQuestions/Q9/com/company/analytics/hr/EmployeeReport.java
+++
@@ -1,7 +0,0 @@
-package com.gla.packagesQuestions.Q9.com.company.analytics.hr;
-
-public class EmployeeReport {
-    public void report(){
-        System.out.println("Employee performance good");
-    }
-}
