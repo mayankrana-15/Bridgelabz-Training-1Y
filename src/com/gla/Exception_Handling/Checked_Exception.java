84e74fcec9c76fd22931d05ff60ca3da6366c933
Mayank Rana <mr9536893@gmail.com>
2026-03-22 17:31:06 +0530

    Assignment 12

---
+++ src/com/gla/Exception_Handling/Checked_Exception.java
@@ -0,0 +1,20 @@
+package com.gla.Exception_Handling;
+import java.io.*;
+public class Checked_Exception {
+    public static void main(String[] args) {
+        try {
+            FileReader fr = new FileReader("data.txt");
+            BufferedReader br = new BufferedReader(fr);
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+
+            br.close();
+        }
+        catch (IOException e) {
+            System.out.println("File not found");
+        }
+    }
+}
